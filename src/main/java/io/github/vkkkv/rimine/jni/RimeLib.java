7998df543da126e8c63d6913c51099c7397e74c3
vkkkv <vkkkv@example.com>
2026-02-22 21:07:48 +0800

    Update RimeInputHandler and RimeLib

--- src/main/java/io/github/vkkkv/rimine/jni/RimeLib.java
+++ src/main/java/io/github/vkkkv/rimine/jni/RimeLib.java
@@ -6,5 +6,6 @@ import com.sun.jna.Native;
 public interface RimeLib extends Library {
   RimeLib INSTANCE = Native.load("rime", RimeLib.class);
 
+  // Only exported function - returns the API structure with all function pointers
   RimeApi.ByReference rime_get_api();
 }
