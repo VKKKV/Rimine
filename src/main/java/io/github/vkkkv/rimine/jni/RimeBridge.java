b934db8ad6c02d8dcacdb135ef09f813edd9c01b
vkkkv <104302767+VKKKV@users.noreply.github.com>
2026-02-22 20:50:57 +0800

    Remove deprecated RimeBridge and bridge.cpp

--- src/main/java/io/github/vkkkv/rimine/jni/RimeBridge.java
+++
@@ -1,9 +0,0 @@
-package io.github.vkkkv.rimine.jni;
-
-public class RimeBridge {
-  static {
-    System.loadLibrary("rimine_jni");
-  }
-
-  public static native void initEngine(String sharedDataDir, String userDataDir);
-}
